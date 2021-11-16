f38b662e61f7ec185daf6e98517cdc0cfbce2858
Benard <blackmankriativity@gmail.com>
2021-11-16 02:22:51 +0200

    amhgmasdgk

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import *
 import tkinter.messagebox as tkMessageBox
 import mysql.connector
@@ -185,12 +186,89 @@ def register():
 
 
 #create product
+def addProductCommit(root,productId, productName, productDescription,productCategory,productPrice):
+    productId = productId.get()
+    productName = productName.get()
+    productCategory = productCategory.get()
+    productDescription = productDescription.get()
+    productPrice = productPrice.get()
+    print('check')
+    database()
+    
+    if productId != "":
+        print(1)
+        cursor.reset()
+        cursor.execute("SELECT * FROM `products` WHERE `productId` = %s AND `productName` = %s", (productId,productName,))
+        product = cursor.fetchone()
+        if product is not None:
+            print(product)
+            y = [product[0],product[1],product[2],product[3],product[4]]
+        cursor.reset()
+        cursor.execute("SELECT * FROM `products` WHERE `productId` = %s", (productId,))
+        product = cursor.fetchone()
+        if product is not None:
+            print(3)
+            product = cursor.fetchone()
+            y = [product[0],product[1],product[2],product[3],product[4]]
+        curItem = productlist.focus()
+        productId.delete(0,"end")
+        productId.insert(0,y[0])
+        productName.delete(0,"end")
+        productName.insert(0,y[0])
+        productDescription.delete(0,"end")
+        productDescription.insert(0,)
+        productCategory.delete(0,"end")
+        productCategory.insert(0,y[0])
+        productPrice.delete(0,"end")
+        productPrice.insert(0,y[0])    
+    else:
+        if productId == "" or productName == "" or productDescription == "" or productCategory == "" or  productPrice== "" :
+            result = tkMessageBox.showerror('Error', 'You need to fill all fields')
+        else:
+            cursor.reset()
+            cursor.execute("SELECT * FROM `products` WHERE `productId` = %s", (productId,))
+            if cursor.fetchone() is not None:
+                result = tkMessageBox.showerror('Error', 'ProductId already Exists', icon="warning") 
+            cursor.reset()
+            cursor.execute("SELECT * FROM `products` WHERE `productName` = %s ", (productName,))
+            if cursor.fetchone() is not None:
+                result = tkMessageBox.showerror('Error', 'productName Exists', icon="warning") 
+            else:
+            
+                cursor.reset() 
+                cursor.execute("INSERT INTO `products` (productId,productName,productDescription,productCategory,productPrice) VALUES( %s, %s, %s, %s, %s)", (str(productId), str(productName), str(productDescription), str(productCategory), str(productPrice)))      
+                conn.commit()
+                cursor.reset()
+                cursor.execute("SELECT * FROM `products` WHERE `productId` = %s", (productId,))
+                y = ''
+                if cursor.fetchone() is not None:
+                    x = cursor.fetchone()
+                    y = productId
+                result = tkMessageBox.showinfo('System', 'Registration for product ' + productName + 'was successfull. Your product ID is ' + y)
+                if result == 'ok':
+                    switch1(root) 
+        cursor.close()
+        conn.close()
 
+def selectItem(productlist,productId, productName,  productDescription,productCategory,productPrice):
+    curItem = productlist.focus()
+    productId.delete(0,"end")
+    productId.insert(0,productlist.item(curItem)['values'][0])
+    productName.delete(0,"end")
+    productName.insert(0,productlist.item(curItem)['values'][1])
+    productDescription.delete(0,"end")
+    productDescription.insert(0,productlist.item(curItem)['values'][2])
+    productCategory.delete(0,"end")
+    productCategory.insert(0,productlist.item(curItem)['values'][3])
+    productPrice.delete(0,"end")
+    productPrice.insert(0,productlist.item(curItem)['values'][4])
+    
+    
 def addProduct(): 
     root = tk.Tk()
     root.geometry("1500x1000")
     root.title("Clutch Center Motor Spares")
-    
+    database()
     lblsecrow = tk.Label(root, text ="CLutch Center Registry", fg = 'blue',)
     lblsecrow.place(x = 170, y = 10)
     # Defining the first row
@@ -201,47 +279,70 @@ def addProduct():
 
     lblsecrow = tk.Label(root, text ="Product Name:", fg = 'blue', )
     lblsecrow.place(x = 120, y = 100)
-    ProductName = tk.Entry(root, width = 35)
-    ProductName.place(x = 250, y = 100, width = 150)
+    productName = tk.Entry(root, width = 35)
+    productName.place(x = 250, y = 100, width = 150)
 
     lblrow6 = tk.Label(root, text ="Product Cartegory", fg = 'blue',)
     lblrow6.place(x = 120, y = 150)
-    ProductCartegory = tk.Entry(root, width = 35)
-    ProductCartegory.place(x = 250, y = 150, width = 150)
+    productCategory = tk.Entry(root, width = 35)
+
+    productCategory.place(x = 250, y = 150, width = 150)
 
     lblrow3 = tk.Label(root, text ="Product Description", fg = 'blue',)
     lblrow3.place(x = 120, y = 200)
-    ProductDescription = tk.Entry(root, width = 35)
-    ProductDescription.place(x = 250, y = 200, width = 150)
+    productDescription = tk.Entry(root, width = 35, )
+    productDescription.place(x = 250, y = 200, width = 150)
+    
 
     lblrow4 = tk.Label(root, text ="Product Price", fg = 'blue',)
     lblrow4.place(x = 120, y = 250)
-    ProductPrice = tk.Entry(root, width = 35)
-    ProductPrice.place(x = 250, y = 250, width = 150)
+    productPrice = tk.Entry(root, width = 35)
+    productPrice.place(x = 250, y = 250, width = 150)
 
-    lblrow5 = tk.Label(root, text ="Option:", fg = 'blue',)
-    lblrow5.place(x = 120, y = 300)
-    password1 = tk.Entry(root, width = 35)
-    password1.place(x = 250, y = 300, width = 150)
-
-
-    # listbox = Listbox(root, bg, fg, bd, height, width, font, ..)
-    productlist = Listbox(root, fg = 'blue',)
-    productlist.place(x = 550, y = 10, width = 1000, height = 1000)
-
+ 
+    # Using treeview widget
+    productlist = ttk.Treeview(root, selectmode ='browse')
+    productlist.place(x = 550, y = 10, width = 700, height = 600)
+    productlist.bind('<ButtonRelease-1>', lambda eff: selectItem(productlist,productId, productName,  productDescription,productCategory,productPrice))
     scrollbar = Scrollbar(root)
-    for values in range(100):
-        productlist.insert(END, values)
-    listbox.config(yscrollcommand = scrollbar.set)
-    scrollbar.config(command = listbox.yview)
+    scrollbar.place(x = 1250, y = 10, width = 10, height = 600)
+    
+    productlist.config(yscrollcommand = scrollbar.set)
+    scrollbar.config(command = productlist.yview)
+    
+    productlist["columns"] = ("1", "2", "3", "4","5")
+    productlist['show'] = 'headings'
+    productlist.column("1", width = 90, anchor ='c')
+    productlist.column("2", width = 90, anchor ='se')
+    productlist.column("3", width = 90, anchor ='se')
+    productlist.column("4", width = 90, anchor ='se')
+    productlist.column("5", width = 90, anchor ='se')
+    
+    productlist.heading("1", text ="Product Id")
+    productlist.heading("2", text ="Product Name")
+    productlist.heading("3", text ="Product Descr")
+    productlist.heading("4", text ="Product Cat")
+    productlist.heading("5", text ="Product Price")
+    cursor.reset()
+    cursor.execute("SELECT * FROM `products` ")
+    for row in cursor.fetchall():
+        print(row)
+        productlist.insert("", 'end', text ="L1",
+                    values =(row[1],row[2],row[3],row[4],row[5]))
+    
+ 
+   
+    #productlist = Listbox(root, fg = 'blue',)
+    
+    
 
-    submitbtn3 = tk.Button(root, text ="Register",bg ='blue', fg = 'white',command = lambda: registerCommit(root,firstname, lastname, username, position,password,password1))
+    submitbtn3 = tk.Button(root, text ="Create",bg ='blue', fg = 'white',command = lambda: addProductCommit(root,productId, productName,  productDescription,productCategory,productPrice))
     submitbtn3.place(x = 200, y = 350, width = 70)
 
-    submitbtn2 = tk.Button(root, text ="exit",bg ='blue', fg = 'white',command= lambda: exit(root))
+    submitbtn2 = tk.Button(root, text ="Edit",bg ='blue', fg = 'white',command= lambda: exit(root))
     submitbtn2.place(x = 300, y = 350, width = 65)
 
-    submitbtn1 = tk.Button(root, text ="login",fg = 'blue',command= lambda: switch1(root))
+    submitbtn1 = tk.Button(root, text ="back",fg = 'blue',command= lambda: switch1(root))
     submitbtn1.place(x = 300, y = 400, width = 100)
     
 
